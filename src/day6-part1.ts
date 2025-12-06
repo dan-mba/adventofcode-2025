f6c07a264347d1aaa4c4ee6a5818e462f209819f
Dan <35927536+dan-mba@users.noreply.github.com>
2025-12-06 09:48:18 -0500

    day 6 part 1

---
+++ src/day6-part1.ts
@@ -0,0 +1,31 @@
+import { open } from "node:fs/promises";
+import { join } from "node:path";
+import { cwd } from "node:process";
+
+let fh;
+
+try {
+  fh = await open(join(cwd(),"input/day6.practice.txt"), "r");
+  const data = await fh.readFile({encoding: "utf-8"});
+  const lines = data.split("\n").filter(l => l.length > 0);
+  const rows = lines.map(l => l.trim().replace(/  +/g, ' ').split(' '));
+  let totals: number[] = [];
+  for(let col = 0; col < (rows[0]?.length ?? 0); col++) {
+    const colArr = rows.map(r => r[col] ?? "");
+    const op = colArr.pop();
+    const numArr = colArr.map(c => parseInt(c, 10));
+    let total = 0;
+    if (op === "+") {
+      total = numArr.reduce((cur, acc) => cur + acc, 0);
+    } else if (op === "*") {
+      total = numArr.reduce((cur, acc) => cur * acc, 1);
+    }
+    totals.push(total);
+  }
+
+  console.log(totals.reduce((cur, acc) => cur + acc, 0))
+  
+
+} finally {
+  await fh?.close();
+}
